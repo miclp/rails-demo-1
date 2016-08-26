fe444076a6c07ee4f37ddf85a57fa8f7bd34d7f0
miclp <michaelpeimani@gmail.com>
2016-08-26 16:25:27 -0700

    Add routes and db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Manager.create(first_name: "Tommy", last_name: "Dorsey")
+Manager.create(first_name: "John", last_name: "Oates")
+
+Store.create(name: "Walgreens")
+Store.create(name: "Radio Shack")
